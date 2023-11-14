22d6534077193e46844371ddc55c805678f07496
LeiKtl <leilaktl.pro@gmail.com>
2023-11-14 17:51:04 +0100

    💄 Update style + responsive

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,12 @@
 import logo from '../../assets/LOGOcomputerLogo.svg';
+import logoMobile from '../../assets/LOGOmobileLogo.svg'
 import './header.scss';
 
 function Header() {
     return (
         <header>
-            <img src={logo} alt="Logo de Kasa" />
+            <img src={logo} alt="Logo de Kasa" className='logoComputer'/>
+            <img src={logoMobile} alt="Logo de Kasa" className='logoMobile'/>
             <nav>
                 <a href="#">Accueil</a>
                 <a href="#">A propos</a>
