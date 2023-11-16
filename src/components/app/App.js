391ee68c5b7925101cb270caa615e220598cfa11
LeiKtl <leilaktl.pro@gmail.com>
2023-11-16 14:52:55 +0100

    🎨 Add footer part

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from '../header';
 import Home from '../../pages/home';
+import Footer from '../footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
