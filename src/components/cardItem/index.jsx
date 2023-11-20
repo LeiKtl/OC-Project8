f7021eb2d844a716600900fb520dac2a79b90e11
LeiKtl <leilaktl.pro@gmail.com>
2023-11-20 17:07:33 +0100

    🎨 Improve structure to add the linear-gradient

--- src/components/cardItem/index.jsx
+++ src/components/cardItem/index.jsx
@@ -3,6 +3,7 @@ import './cardItem.scss';
 function CardItem({cover, title}) {
     return (
         <li className='cardItem'>
+            <div className='gradient'></div>
             <img src={cover} alt={`${title} cover`} />
             <h2>{title}</h2>
         </li>
