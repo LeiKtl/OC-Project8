21cd7540949100f338fe72f2f773a88fd577b180
LeiKtl <leilaktl.pro@gmail.com>
2023-11-21 17:22:45 +0100

    🎨 Add new code at the root for about + error page

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,23 @@
 import './App.scss';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../components/header';
 import Home from '../pages/home';
-import Footer from '../components/footer'
+import Sheet from '../pages/accomodationSheet';
+import About from '../pages/about';
+import Error from '../pages/error'
+import Footer from '../components/footer';
+import { lodgingList } from '../datas/lodgingList';
 
 function App() {
   return (
-    <div className="Router">
+    <div className="App">
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+            <Route path="housing" element={<Sheet src="" />}/>
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </Router>
