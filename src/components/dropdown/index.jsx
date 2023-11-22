5b7c2e2022c8c2ed96d1708d83dd5a586acb569a
LeiKtl <leilaktl.pro@gmail.com>
2023-11-22 19:56:22 +0100

    🎨 Improve code for the dropdown

--- src/components/dropdown/index.jsx
+++ src/components/dropdown/index.jsx
@@ -1,13 +1,24 @@
 import './dropdown.scss';
 import arrow from '../../assets/arrowDropdown.svg';
+import { useState } from 'react';
 
 function Dropdown(props) {
+
+    const [isClosed, setIsClosed] = useState(true);
+
+    function toggle() {
+        setIsClosed(!isClosed);
+    };
+
     return (
-        <div className='dropdown'>
-            <h2>{props.title}</h2>
-            <img src={arrow} alt="Flèche pour ouvrir le contenant" />
+        <div className='containerDropdown'>
+            <div className='dropdownBtn' onClick={() => toggle()}>
+                <h2>{props.title}</h2>
+                <img src={arrow} alt="Flèche pour ouvrir le contenant" />
+            </div>
+            <p className={isClosed ? 'content hide' : 'content'}>{props.textContent}</p>
         </div>
-    )
+    ) 
 };
 
 export default Dropdown;
