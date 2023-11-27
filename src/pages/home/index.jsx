fadbcd8f2715e7c6cea2826bf4ab9ddca96cf02c
LeiKtl <leilaktl.pro@gmail.com>
2023-11-27 14:46:32 +0100

    🚚

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,13 +2,13 @@ import './home.scss';
 import bannerHome from '../../assets/homeBanner.svg';
 import Banner from '../../components/banner';
 import CardsList from '../../components/cardsList';
-import { lodgingList } from '../../datas/lodgingList';
+import { housingList } from '../../datas/housingList';
 
 function Home() {
     return (
         <main>
             <Banner src={bannerHome} alt="Bannière de la page d'acceuil" tagLine="Chez vous, partout et ailleurs"/>
-            <CardsList data={lodgingList}/>
+            <CardsList data={housingList}/>
         </main>
     )
 };
